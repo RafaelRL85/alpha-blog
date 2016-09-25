6b7904f5650f0ea6e769b99dee3010f662061fb6
Rafa Ramirez <rapharl85@gmail.com>
2016-09-25 13:47:08 -0700

    add styiling to the flash messages and converted to partials

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,7 +16,7 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     if @article.save
-      flash[:notice] = 'Article was created!'
+      flash[:success] = 'Article was created!'
       redirect_to article_path(@article)
     else
       render 'new'
@@ -31,14 +31,14 @@ class ArticlesController < ApplicationController
   def destroy
 
     @article.destroy
-    flash[:notice] = "Article has been deleted"
+    flash[:danger] = "Article has been deleted"
     redirect_to articles_path
   end
 
   def update
 
     if @article.update(article_params)
-      flash[:notice] = "Article has been updated"
+      flash[:success] = "Article has been updated"
       redirect_to article_path(@article)
     else
       render 'edit'
