85e85080ff49c7dd3205b1de4861c424f9c2f36e
Rafa Ramirez <rapharl85@gmail.com>
2016-10-01 17:03:34 -0700

    restricted actions on the controllers with before action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,9 @@
 class ArticlesController < ApplicationController
 
   before_action :set_article, only: [:edit, :show, :update, :destroy]
+  before_action :require_user, except: [:index, :show]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
+  
   def index
     @articles = Article.paginate(page: params[:page], per_page: 3)
   end
@@ -51,8 +54,15 @@ class ArticlesController < ApplicationController
   def set_article
     @article = Article.find(params[:id])
   end
-    def article_params
-      params.require(:article).permit(:title, :description)
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
+
+  def require_same_user
+    if current_user != @article.user
+      flash[:danger] = "This is not your article, please do not try to modify other people's articles"
+      redirect_to root_path
     end
+  end
   
 end
